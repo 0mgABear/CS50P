49503222edbb478201d36eddd399b085b9206426
0mgABear <yeoyi94@gmail.com>
2024-07-19 20:59:45 +0800

    pset5

---
+++ Week5/Problem Set 5/test_bank.py
@@ -0,0 +1,13 @@
+from bank import value
+
+def test_value():
+    assert value("Hello") == 0
+    assert value("Hello, Newman") == 0
+    assert value("How you doing?") == 20
+    assert value("What's happening") == 100
+    assert value("hi there") == 20
+    assert value("") == 100
+    assert value("  HELLO  ") == 0
+    assert value("hey") == 20
+
+
